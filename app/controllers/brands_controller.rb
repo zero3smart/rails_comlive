3878770be676f1755a9d33c65966df661bc7d678
kuissoftech <fivewisdomzero@gmail.com>
2017-09-26 14:30:07 -0400

    fixed bug in brands#show

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -21,7 +21,7 @@ class BrandsController < ApplicationController
 
   def show
     @brand = @app.brands.find(params[:id])
-    # @standardization = Standardization.new
+    @standardization = Standardization.new
   end
 
   def edit
