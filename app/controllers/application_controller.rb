d70b2be6224241286c9a8320f9ef9148fda6d56c
kuissoftech <fivewisdomzero@gmail.com>
2017-09-26 16:28:25 -0400

    redirect to last visited app on signin, show apps alongside user avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,26 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  after_filter :last_accessed_app
+
+
+  helper_method :current_app
+
+  def current_app
+    App.find_by(id: params[:app_id] || params[:id])
+  end
+
+
+  def last_accessed_app
+    return unless request.get?
+    return unless request.path.match(/\/apps\/(\d+)/)
+    cookies.permanent[:last_app_id] = request.path.match(/\/apps\/(\d+)/)[1]
+  end
+
+  def after_sign_in_path_for(resource)
+    return root_path unless cookies[:last_app_id]
+    app = App.find_by(id: cookies[:last_app_id])
+    return root_path if app.nil?
+    app_path(app)
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,7 @@ module ApplicationHelper
 
   def errors_for(object)
     if object.errors.any?
-      content_tag :div, class: "alert alert-dismissable alert-danger hidden" do
+      content_tag :div, class: "alert alert-dismissable alert-danger" do
         concat '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>'.html_safe
         concat content_tag(:h3, "Oh Snap!")
         object.errors.full_messages.each do |msg|
@@ -32,29 +32,5 @@ module ApplicationHelper
   def gravatar_for(email, size = 50, classes = "")
     hash = Digest::MD5.hexdigest(email)
     image_tag "https://www.gravatar.com/avatar/#{hash}?s=#{size}&d=identicon", class: classes
-  end
-
-  def options_for_visibility(record)
-    descriptions = {
-        publicized: { text: t("publishing.public").html_safe, title: "Public" },
-        privatized: { text: t("publishing.private"), title: "Private"  },
-        official: { text: t("publishing.official"), title: "Official" }
-    }
-    record.class.visibilities.map do |k,v|
-      [descriptions[k.to_sym][:title], k, data: { subtext: descriptions[k.to_sym][:text]}]
-    end
-  end
-
-  def errors_for_field(model,attribute)
-    if model.errors[attribute].present?
-      content_tag :span, class: 'help-block' do
-        model.errors[attribute].join(", ")
-      end
-    end
-  end
-
-  def initials_for(app)
-    return unless app.valid?
-    app.name.chars.first.upcase
   end
 end
