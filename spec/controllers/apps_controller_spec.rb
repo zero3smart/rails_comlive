503492a43af4e23f8bc4ab840360c7736a0eb3e5
pokesoftech <fivewisdomzero@gmail.com>
2017-10-25 12:54:13 -0400

    merged layer-b2n fixed test suite

--- spec/controllers/apps_controller_spec.rb
+++ spec/controllers/apps_controller_spec.rb
@@ -1,52 +1,89 @@
 require 'rails_helper'
 
 RSpec.describe AppsController, :type => :controller do
-  before(:each) do
-    #@request.env["devise.mapping"] = Devise.mappings[:user]
-    setup_knock do |user|
-      @user = create(:user)
-    end
-    #sign_in @user
-  end
+  let!(:user) { create(:user) }
+  let(:app) { create(:app, user: user) }
 
-  describe "GET #index" do
-    it "returns 200 http status code" do
-      get :index
-      expect(response.status).to eq 200
+  context "As an authenticated user" do
+    before(:each) do
+      sign_in(user)
     end
-  end
 
-  describe "GET #show" do
-    it "returns 200 http status code" do
-      app = create(:app, user_id: @user.id)
-      get :show, params: { id: app }
-      expect(response.status).to eq 200
-    end
-  end
-
-  describe "GET #new" do
-    it "returns 200 http status code" do
-      get :new
-      expect(response.status).to eq 200
-    end
-  end
-
-  describe "POST #create" do
-    context "with valid attributes" do
-      it "saves the new app in the database" do
-        expect{
-          post :create, params: { app: attributes_for(:app) }
-        }.to change(App, :count).by(1)
+    describe "GET #index" do
+      it "returns 200 http status code" do
+        get :index
+        expect(response.status).to eq 200
       end
     end
 
-    context "with invalid attributes" do
-      it "does not save the new app in the database" do
-        expect{
-          post :create, params: { app: attributes_for(:invalid_app)}
-        }.not_to change(App, :count)
+    describe "GET #show" do
+      it "returns 200 http status code" do
+        get :show, params: { id: app }
+        expect(response.status).to eq 200
+      end
+    end
+
+    describe "GET #new" do
+      it "returns 200 http status code" do
+        get :new
+        expect(response.status).to eq 200
+      end
+    end
+
+    describe "POST #create" do
+      context "with valid attributes" do
+        it "saves the new app in the database" do
+          expect{
+            post :create, params: { app: attributes_for(:app) }
+          }.to change(App, :count).by(1)
+        end
+      end
+
+      context "with invalid attributes" do
+        it "does not save the new app in the database" do
+          expect{
+            post :create, params: { app: attributes_for(:invalid_app)}
+          }.not_to change(App, :count)
+        end
       end
     end
   end
 
+  context "As an unauthenticated user" do
+    describe "GET #index" do
+      it "returns 302 http status code" do
+        get :index
+        expect(response.status).to eq 302
+        expect(response).to redirect_to(login_path)
+        expect(flash[:alert]).to eq("You need to sign in or sign up before continuing.")
+      end
+    end
+
+    describe "GET #show" do
+      it "returns 302 http status code" do
+        get :show, params: { id: 1 }
+        expect(response.status).to eq 302
+        expect(response).to redirect_to(login_path)
+        expect(flash[:alert]).to eq("You need to sign in or sign up before continuing.")
+      end
+    end
+
+    describe "GET #new" do
+      it "returns 302 http status code" do
+        get :new
+        expect(response.status).to eq 302
+        expect(response).to redirect_to(login_path)
+        expect(flash[:alert]).to eq("You need to sign in or sign up before continuing.")
+      end
+    end
+
+    describe "POST #create" do
+      it "returns 302 http status code" do
+        post :create, params: { app: attributes_for(:app) }
+        expect(response.status).to eq 302
+        expect(response).to redirect_to(login_path)
+        expect(flash[:alert]).to eq("You need to sign in or sign up before continuing.")
+      end
+    end
+  end
 end
--- spec/controllers/auth0_controller_spec.rb
+++ spec/controllers/auth0_controller_spec.rb
@@ -2,17 +2,26 @@ require 'rails_helper'
 
 RSpec.describe Auth0Controller, :type => :controller do
 
-  describe "GET callback" do
-    it "returns http success" do
-      get :callback
-      expect(response).to have_http_status(:success)
+  describe "GET #callback" do
+    before(:each) do
+      setup_omniauth_mock
+      request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:auth0]
+    end
+
+    it "logs in the user" do
+      get :callback, params: { code: "ksdf89sdf" }
+      expect(response.status).to eq 302
+      expect(response).to redirect_to(root_path)
+      expect(flash[:notice]).to eq("Signed in successfully")
     end
   end
 
-  describe "GET failure" do
-    it "returns http success" do
-      get :failure
-      expect(response).to have_http_status(:success)
+  describe "GET #failure" do
+    it "redirects to root path with error message" do
+      get :failure, params: { message: "Invalid Credentials" }
+      expect(response.status).to eq 302
+      expect(response).to redirect_to(root_path)
+      expect(flash[:alert]).to eq("Invalid Credentials")
     end
   end
 
